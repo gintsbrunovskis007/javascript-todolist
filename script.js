32ae310b86276abf220ded892b8d0e8dd342acd6
Gints Brunovskis <brunovskis.gints007@gmail.com>
2026-09-05 20:55:08 +0300

    feat: add delete button to completed tasks

--- script.js
+++ script.js
@@ -14,15 +14,25 @@ tasks.forEach((task) => {
 
   checkbox.checked = task.completed;
 
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
   li.appendChild(checkbox);
   li.append(task.text);
 
   if (!task.completed) {
     activeTaskList.appendChild(li);
   } else {
+    li.append(deleteButton);
     completedTaskList.appendChild(li);
     li.classList.add("completed");
   }
+
+  deleteButton.addEventListener("click", () => {
+    tasks = tasks.filter((t) => t !== task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    li.remove();
+  });
 
   checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked;
@@ -31,6 +41,7 @@ tasks.forEach((task) => {
       activeTaskList.appendChild(li);
       li.classList.remove("completed");
     } else {
+      li.append(deleteButton);
       completedTaskList.appendChild(li);
       li.classList.add("completed");
     }
@@ -53,6 +64,9 @@ taskForm.addEventListener("submit", (e) => {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
 
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
   li.appendChild(checkbox);
   li.append(taskInputValue);
 
@@ -63,6 +77,12 @@ taskForm.addEventListener("submit", (e) => {
     completed: false,
   };
 
+  deleteButton.addEventListener("click", () => {
+    tasks = tasks.filter((t) => t !== task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    li.remove();
+  });
+
   checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked;
 
@@ -70,6 +90,7 @@ taskForm.addEventListener("submit", (e) => {
       activeTaskList.appendChild(li);
       li.classList.remove("completed");
     } else {
+      li.append(deleteButton);
       completedTaskList.appendChild(li);
       li.classList.add("completed");
     }
