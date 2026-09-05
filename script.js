2c73d9265931fb0ae0e5dbe4e25e1fab50e6ca19
Gints Brunovskis <brunovskis.gints007@gmail.com>
2026-09-05 19:16:39 +0300

    feat: save task to local storage

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const taskInput = document.getElementById("task-input");
 const taskCreateButton = document.getElementById("task-create-button");
 const taskList = document.getElementById("task-list");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+tasks.forEach((task) => {
+  const li = document.createElement("li");
+  li.textContent = task;
+  taskList.appendChild(li);
+});
+
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -16,6 +24,9 @@ taskForm.addEventListener("submit", (e) => {
   li.textContent = taskInputValue;
 
   taskList.appendChild(li);
+
+  tasks.push(taskInputValue);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
   taskForm.reset();
 });
